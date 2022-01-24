0d04020030ecb0667c523346a1aa94f1775663a7
izzahaj <izzah.jalil02@gmail.com>
2022-01-25 01:45:21 +0800

    Removed rack cors

--- config/application.rb
+++ config/application.rb
@@ -34,16 +34,6 @@ module Tache
 
     # Don't generate system test files.
     config.generators.system_tests = nil
-
-    Rails.application.config.middleware.insert_before 0, Rack::Cors do
-      allow do
-        origins '*'
-    
-        resource '*',
-          headers: :any,
-          methods: [:get, :post, :put, :patch, :delete, :options, :head]
-      end
-    end
     
   end
 end
